32171f8c93b2c4dcfe334eb1af69c99fbc531295
root <root@workspacexzvd52mco81t61ei.dockerimage-6b694796f7-6zf9c>
2019-01-09 18:53:13 +0000

    changes

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import uuidv4 from 'uuid/v4';
 import './Todo.css';
+
 class Todo extends Component {
 constructor() {
 super();
@@ -10,22 +11,12 @@ task: '',
 items: []
 };
 }
-handleOnChange = e => {
-const { target: { value } } = e;
-// Updating our task state with the input value...
-this.setState({
-task: value
-});
-}
 render() {
 return (
 <div className="Todo">
 <h1>New Task:</h1>
 <form onSubmit={this.handleOnSubmit}>
-<input
-value={this.state.task}
-onChange={this.handleOnChange}
-/>
+<input value={this.state.task} />
 </form>
 </div>
 );
